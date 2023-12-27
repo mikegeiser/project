ef0524718054937da0685bd90e6fb1e41d2dfc49
Michael Geiser <michaelgeiser@Michaels-MacBook-Pro-6.local>
2023-12-27 14:41:56 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Study the role of safe haven in the investment process for institutional clients',
+    author='Michael Geiser',
+    license='BSD-3',
+)
